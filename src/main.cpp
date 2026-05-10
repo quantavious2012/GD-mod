dba4a4e4b614ef47c2e4a6fc5246668231b1030d
quantavious2012 <baxterplonske@gmail.com>
2026-05-10 13:38:21 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,11 @@ using namespace geode::prelude;
 
 class $modify(MenuLayer) {
     bool init() {
-        if (!MenuLayer::init()) return false;
-        
-        log::info("Hi from Geode Android!");
-        
+        if (!MenuLayer::init()) {
+            return false;
+        }
+
+        log::info("Hello from Android!");
         return true;
     }
 };
